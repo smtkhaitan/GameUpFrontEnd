93eef17508f9d989f37d4d8a00cc4509e5b84ba0
Aditya-Kalyani-221 <adiakakalyani@gmail.com>
2019-03-09 14:40:25 +0530

    angular add

---
+++ js/maincontroller.js
@@ -0,0 +1,7 @@
+var app=angular.module('myApp', []);
+    app.controller('maincontroller', function($scope) {
+    $scope.name = "John";
+    $scope.dob = 1552104170555;
+
+    console.log("Inside");
+});
