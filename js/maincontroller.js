c1b8777771b16c675255fcb1d28d72a5640169cc
Aditya-Kalyani-221 <adiakakalyani@gmail.com>
2019-03-10 02:30:31 +0530

    MyCommit

--- js/maincontroller.js
+++ js/maincontroller.js
@@ -1,10 +1,11 @@
 var app=angular.module('myApp', []);
-    app.controller('maincontroller', function($scope,$http) {
+    app.controller('maincontroller', function($scope,$http,$window) {
     $scope.candidate = {};
     $scope.confirmPassword = "";
     $scope.errorMsgShow = false;
     $scope.names = ["Male", "Female", "Other"];
     $scope.agegrp = ["15-20", "21-25", "26-30", "31-35", "36-40", "41-45", "46-50", "51-55"];
+    $scope.can = {};
     $scope.signUpCandidate = function(){
         if($scope.candidate.gender === "Male") {
             $scope.candidate.gender = 'm';
@@ -23,9 +24,49 @@ var app=angular.module('myApp', []);
         var url = 'http://10.177.7.29:8083/v1/gameUp/signUp', data = $scope.candidate ,config='JSON';
         $http.post(url, data, config).then(function (response) {
             console.log("ghar challo");
+            $window.location.href = '/GameUpFrontEnd/index.html';
         }, function (response) {
             console.log("chud gya");
+            $window.location.href = '/GameUpFrontEnd/index.html';
         });
         console.log($scope.candidate);
+    };
+
+    $scope.geoLocation = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+        }
+    };
+
+    function showPosition(position) {
+        $scope.latitude = position.coords.latitude;
+        $scope.longitude = position.coords.longitude;
+        if($scope.latitude !== null) {
+            $scope.allowLogin = true;
+        }
     }
-});
+
+    $scope.logInCandidate = function() {
+        if($scope.allowLogin !== true) {
+            return;
+        }
+        else {
+
+        }
+
+    }
+
+        $scope.logInCandidate = function () {
+            $scope.can.latitude = "123";
+            $scope.can.longitude = "123";
+            var url = 'http://10.177.7.29:8083/v1/gameUp/login', data = $scope.can, config = 'JSON';
+            $http.post(url, data, config).then(function (response) {
+                console.log("ghar challo = " + JSON.stringify(response.data));
+                // $window.location.href = '/GameUpFrontEnd/maps.html';
+            }, function (response) {
+                console.log("chud gya = " + JSON.stringify(response.data));
+            });
+            console.log("Can = " + JSON.stringify($scope.can));
+        }
+    });
